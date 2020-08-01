33026c3a9c9829335fba5473d28f95d9ef8970e3
spysappin <spysappin98@gmail.com>
2020-08-01 17:34:58 +0200

    fixed requests when user wasn't logged in

--- db/createdb.js
+++ db/createdb.js
@@ -2,40 +2,45 @@
 DB_HOST=examplehost
 DB_USER=exampleuser
 DB_PASS=examplepass
+DB_DATABASE_NAME=dbname
 */
 
 var mysql = require('mysql');
 
 const path = require('path')
-require('dotenv').config({path: path.resolve(__dirname,'.env')})
+console.log(path.resolve(__dirname,'../.env'));
+require('dotenv').config({path: path.resolve(__dirname,'../.env')})
 
 var con = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE_NAME //remove this when first running the file, then include again
 });
 
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
-  con.query("CREATE DATABASE IF NOT EXISTS photomap", function (err, result) {
+  con.query("CREATE DATABASE IF NOT EXISTS "+process.env.DB_DATABASE_NAME, function (err, result) {
     if (err) throw err;
+    con.query("USE "+process.env.DB_DATABASE_NAME, (error, results, fields) => {
+    });
     console.log("Database created");
   });
-  con.query("CREATE TABLE IF NOT EXISTS markers(id int(10) primary key auto_increment, name varchar(150), description text, lat float(10,6), lon float(10,6)  )", function (err, result) {
+  con.query("CREATE TABLE IF NOT EXISTS markers(id int(10) primary key auto_increment, name varchar(150), description text, lat float(10,6), lon float(10,6), user int(10)  )", function (err, result) {
     if (err) throw err;
-    console.log("Database created");
+    console.log("markers created");
   });
-  con.query("CREATE TABLE IF NOT EXISTS photos(id varchar(100), name varchar(100), marker int(10) )", function (err, result) {
+  con.query("CREATE TABLE IF NOT EXISTS photos(id varchar(100), name varchar(100), marker int(10), user int(10) )", function (err, result) {
     if (err) throw err;
-    console.log("Database created");
+    console.log("photos created");
   });
-  con.query("CREATE TABLE IF NOT EXISTS ratings(id int(10) primary key auto_increment, marker int(10), user int(10))", function (err, result) {
+  con.query("CREATE TABLE IF NOT EXISTS ratings(id int(10) primary key auto_increment, marker int(10), rating int(10), user int(10))", function (err, result) {
     if (err) throw err;
-    console.log("Database created");
+    console.log("ratings created");
   });
   con.query("CREATE TABLE IF NOT EXISTS users(id int(10) primary key auto_increment, username varchar(10), password binary(60))", function (err, result) {
     if (err) throw err;
-    console.log("Database created");
+    console.log("users created");
   });
 });
--- public/js/script.js
+++ public/js/script.js
@@ -1,15 +1,27 @@
 let promise1 = fetch('/get-markers').then(response=>response.json());
 let promise2 = fetch('/api/user-data').then(response=>response.json());
 
+/*
+let user = promise2.then(function(data){
+    console.log("asd");
+    console.log(data);
+});
+*/
+
+let markers = [];
+let user = {};
+
 Promise.all([promise1,promise2]).then(data=>{
-    const markers = JSON.parse(data[0]);
-    const userdata = data[1];
+    markers = JSON.parse(data[0]);
+    console.log(markers);
+    console.log(markers.length);
+    user = data[1];
 
     console.log(data);
-    setLoginstatus(userdata.username,document.querySelector('#loginstatus'));
+    setLoginstatus(user.username,document.querySelector('#loginstatus'));
 
-    loadMap(markers);
-    console.log("finished loading both fetches:",markers,"\n",userdata);
+    loadMap(markers,user);
+    console.log("finished loading both fetches:",markers,"\n",user);
 })
 
 function setLoginstatus(user,loginstatus){
@@ -23,7 +35,7 @@ function setLoginstatus(user,loginstatus){
 
 let map = L.map('map').setView([47.4874728,19.0474751], 8);
 
-function loadMap(markers){
+function loadMap(markers,user){
 
     mapLink = 
         '<a href="https://openstreetmap.org">OpenStreetMap</a>';
@@ -35,75 +47,61 @@ function loadMap(markers){
 
 
     for (let i = 0; i < markers.length; i++) {
-        marker = new L.marker([markers[i].lat,markers[i].lon])
+        if (markers[i].user==user.id){
+            marker = new L.marker([markers[i].lat,markers[i].lon])
+                .bindPopup(
+                    "<p>"+markers[i].name+"</p>"+"<p>"+markers[i].description+"</p>"+
+                    "<button class='delete_button' name='"+markers[i].id+"'>delete</button>"+"<br>"+
+                    "<button class='modify_button' name='"+markers[i].id+"'>modify</button><br>"+
+                    "<a href='/id?id="+markers[i].id+"'>link</a>")
+                .addTo(map);
+        } else {
+            marker = new L.marker([markers[i].lat,markers[i].lon])
             .bindPopup(
-                markers[i].name+"<br>"+markers[i].description+"<br>"+
-                "<button class='delete_button' name='"+markers[i].id+"'>delete</button>"+"<br>"+
-                "<button class='modify_button' name='"+markers[i].id+"'>modify</button><br>"+
-                "<a href='https://dev.adrianturtoczki.com/id?id="+markers[i].id+"'>link</a>")
+                "<p>"+markers[i].name+"</p>"+"<p>"+markers[i].description+"</p>"+
+                "<a href='/id?id="+markers[i].id+"'>link</a>")
             .addTo(map);
+        }
     }
 }
 
     map.on('click', function(e) {
         console.log("test");
-        console.log(user.user);
+
         if (confirm("Do you want to place a marker here?")){
             let name = prompt("name of marker");
             let description = prompt("description of marker");
 
-            console.log(user);
-            const data_to_send = {id:markers[markers.length-1].id+1,name:name,description:description,lat:e.latlng.lat,lng:e.latlng.lng};
-
-            console.log(data_to_send);
-
-            fetch('https://dev.adrianturtoczki.com/add-marker',{
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(data_to_send)
-                }).then(response=>{
-                console.log(response);
-                }).catch(error=>console.error(error));
-        }
-    } );
+                    console.log(markers);
+                    const data_to_send = {name:name,description:description,lat:e.latlng.lat,lng:e.latlng.lng};
+                    console.log(data_to_send);
+    
+    
+                    fetch('/add-marker',{
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify(data_to_send)
+                        }).then(response=>{
+                        console.log(response);
+                        }).catch(error=>console.error(error));
+    }
+});
 
     map.on('popupopen',function(){
         const delete_button = document.querySelector('.delete_button');
-        delete_button.addEventListener('click', function(e) {
-            e.preventDefault();
-            console.log('button '+delete_button.getAttribute("name")+' was clicked');
-
-            const data_to_send = {id:delete_button.getAttribute("name")};
-
-            //console.log(data_to_send);
-
-            fetch('/delete-marker',{
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(data_to_send)
-                }).then(response=>{
-                console.log(response);
-                }).catch(error=>console.error(error));
-
-        });
-
-        const modify_button = document.querySelector('.modify_button');
-        modify_button.addEventListener('click', function(e) {
-            let modify_type = prompt("write 'name' to modify name, 'description' to modify description, 'lat' to modify latitude, 'lon' to modify longitude");
-            let modify_to = prompt("What do you want to modify it to?");
-            e.preventDefault();
-            console.log('button '+modify_button.getAttribute("name")+' was clicked');
-
-            const data_to_send = {modify_type:modify_type,modify_to:modify_to,id:modify_button.getAttribute("name")};
-
-            console.log(data_to_send);
-
-            if ((modify_type=="name"||modify_type=="description"||modify_type=="lat"||modify_type=="lon")&&modify_to!=""){
-                fetch('/modify-marker',{
+        console.log("check if delete exists:"+delete_button);
+        if (delete_button&&delete_button.getAttribute("name")==user.id){
+            delete_button.addEventListener('click', function(e) {
+                e.preventDefault();
+                console.log('button '+delete_button.getAttribute("name")+' was clicked');
+    
+                const data_to_send = {id:delete_button.getAttribute("name")};
+    
+                //console.log(data_to_send);
+    
+                fetch('/delete-marker',{
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
@@ -112,8 +110,33 @@ function loadMap(markers){
                     }).then(response=>{
                     console.log(response);
                     }).catch(error=>console.error(error));
-            } else {
-                console.log("Error: wrong modify_type or empty modify_to")
-            }
-        });
+    
+                    const modify_button = document.querySelector('.modify_button');
+                    modify_button.addEventListener('click', function(e) {
+                        let modify_type = prompt("write 'name' to modify name, 'description' to modify description, 'lat' to modify latitude, 'lon' to modify longitude");
+                        let modify_to = prompt("What do you want to modify it to?");
+                        e.preventDefault();
+                        console.log('button '+modify_button.getAttribute("name")+' was clicked');
+            
+                        const data_to_send = {modify_type:modify_type,modify_to:modify_to,id:modify_button.getAttribute("name")};
+            
+                        console.log(data_to_send);
+            
+                        if ((modify_type=="name"||modify_type=="description"||modify_type=="lat"||modify_type=="lon")&&modify_to!=""){
+                            fetch('/modify-marker',{
+                                method: 'POST',
+                                headers: {
+                                    'Content-Type': 'application/json',
+                                },
+                                body: JSON.stringify(data_to_send)
+                                }).then(response=>{
+                                console.log(response);
+                                }).catch(error=>console.error(error));
+                        } else {
+                            console.log("Error: wrong modify_type or empty modify_to")
+                        }
+                    });
+            });
+        }
+    
     });
